6f8ec22dfc835462b4ad3c051b264a0fa7b2c739
emmanuel-odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2024-04-17 13:56:42 +0000

    feat: project setup

---
+++ src/test/java/com/springboot/mortgagecalculator/MortgageCalculatorAppTests.java
@@ -0,0 +1,13 @@
+//package com.springboot.mortgagecalculator;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MortgageCalculatorAppTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
